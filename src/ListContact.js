11b77fdd97b5fff20286a865bd329e80df0ab0e7
Azher Saeed Ch <chazher1997@gmail.com>
2020-05-12 16:25:19 +0500

    Data search by the query

--- src/ListContact.js
+++ src/ListContact.js
@@ -1,13 +1,34 @@
 import React from 'react'
-class ListContact extends React.Component{
 
+class ListContact extends React.Component{
+    state = {
+        query : ''
+    }
+
+    handleQuery = (query) => {
+        this.setState(() => ({
+            query : query.trim()
+        }))
+    }
     
     render(){
         const {contacts , DeleteContact } = this.props
+        const { query } = this.state
+
+        const trimContact = query === '' ? contacts : contacts.filter((c) => (
+            c.name.toLowerCase().includes(query.toLowerCase())
+        ))
         return(
-            <ol>
+            <div className="list-contacts">
+                <div className="list-contacts-top">
+                    <input type="text" value={query} 
+                    onChange={(event) => this.handleQuery(event.target.value)}
+                    className="search-contacts" 
+                    placeholder="Search contact"/>
+                </div>
+                 <ol>
                 {
-                    contacts.map((contact) => (
+                    trimContact.map((contact) => (
                         <li key={contact.id} className='contact-list-item'>
                         <div
                           className='contact-avatar'
@@ -27,6 +48,8 @@ class ListContact extends React.Component{
                     ))
                 }
             </ol>
+            </div>
+           
         )
     }
 }
